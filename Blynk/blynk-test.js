25d9ed274f7733e8371c9c4e90db652aa27e535d
Jonathan Ventura <jventura@uccs.edu>
2016-01-09 23:39:42 -0700

    Adding Blynk node.js script

---
+++ Blynk/blynk-test.js
@@ -0,0 +1,79 @@
+
+// Blynk setup
+var Blynk = require('blynk-library');
+
+var AUTH = '94707690fc7a4e04b44894c7f68d9742';
+
+var blynk = new Blynk.Blynk(AUTH, options = {
+  connector : new Blynk.TcpClient( )
+});
+
+blynk.on('connect', function() { console.log("Blynk ready."); });
+blynk.on('disconnect', function() { console.log("DISCONNECT"); });
+
+
+
+// serial port setup
+var SerialPort = require("serialport").SerialPort;
+
+var serialPort = new SerialPort("/dev/cu.usbmodem1421", {
+  baudrate: 115200
+});
+
+
+
+// parameters from Blynk app
+var esc;
+var servo;
+
+// Blynk input virtual pins
+var escpin = new blynk.VirtualPin(0);
+escpin.on('write',function(param) {
+  esc = param;
+});
+
+var servopin = new blynk.VirtualPin(1);
+servopin.on('write',function(param) {
+  servo = param;
+});
+
+
+
+
+// readings from Arduino
+var gyro;
+var pwm;
+
+// Blynk output virtual pins
+var gyropin = new blynk.VirtualPin(2);
+gyropin.on('read',function() {
+  gyropin.write(gyro);
+});
+
+var pwmpin = new blynk.VirtualPin(3);
+pwmpin.on('read',function() {
+  pwmpin.write(pwm);
+});
+
+
+// serial handlers
+serialPort.on("open", function () {
+  console.log('open');
+  serialPort.on('data', function(data) {
+    // input format:
+    // gyro (int16)
+    // pwm (int16)
+    gyro = data.readInt16LE(0);
+    pwm = data.readInt16LE(2);
+  });
+  setInterval( function() {
+    // output format:
+    // esc
+    // servo
+    data = new Buffer(4);
+    data.writeInt16LE(esc,0);
+    data.writeInt16LE(servo,2);
+    serialPort.write(data);
+  },1000);
+});
+
